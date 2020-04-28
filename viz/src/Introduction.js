5a8f52e52f1a21d0501d604721fcde998b05159a
Aaron Ho <12478592+Platinuman@users.noreply.github.com>
2020-04-28 14:06:13 -0400

    Update Intro

--- viz/src/Introduction.js
+++ viz/src/Introduction.js
@@ -9,16 +9,17 @@ const Introduction = () => {
     return (
     <Container maxWidth="md">
       <Typography variant="h2" className={classes.titleClasses}>
-        Introduction
+        COVIM because we KARE
+
+COVID-19 Visualization and Intervention Modeling
+By Kiara, Aaron, Rashid, Eli
       </Typography>
       <Box pt={2} />
       <Typography variant="body1">
-        The COVID-19 pandemic poses a serious health risk to people all over the world. While older adults tend to have more severe symptoms and higher death rates, the complete effects of infection with COVID-19 are still unknown. We will model several mitigation and suppression strategies and compare their effectiveness. By modeling the effects of different interventions, we hope to make helpful visualizations of intervention effects that can be used to inform policy recommendations for how to best respond to COVID-19. Though most major countries have taken some interventions already, our models will be able to help predict when it is safe to ease public health measures, and whether it is necessary to impose more severe ones. Additionally, the models may be helpful for modeling when interventions should first be put into place for future epidemics.
+        The COVID-19 pandemic poses a serious health risk to people all over the world. Many countries are currently imposing public health interventions that include stay-at-home orders, social distancing measures, or contact tracing. These efforts help to reduce the load on hospital systems and the total number of deaths. Our model can be used to illustrate why these public health interventions have been employed, and show the consequences of returning to normal too quickly. Furthermore, our model can help predict when it is safe to ease public health measures, and whether it is necessary to impose more severe ones. Beyond COVID-19, our model may be helpful for modeling when interventions should first be put into place for future pandemics.
       </Typography>
       <Box pt={2} />
-      <Typography variant="body1">
-        The COVID-19 pandemic poses a serious health risk to people all over the world. While older adults tend to have more severe symptoms and higher death rates, the complete effects of infection with COVID-19 are still unknown. We will model several mitigation and suppression strategies and compare their effectiveness. By modeling the effects of different interventions, we hope to make helpful visualizations of intervention effects that can be used to inform policy recommendations for how to best respond to COVID-19. Though most major countries have taken some interventions already, our models will be able to help predict when it is safe to ease public health measures, and whether it is necessary to impose more severe ones. Additionally, the models may be helpful for modeling when interventions should first be put into place for future epidemics.
-      </Typography>
+
     </Container>
   );
 }
