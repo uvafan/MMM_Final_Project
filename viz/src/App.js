8affb644bfa02f1751b0d049f5800f86a6a7af9e
Rashid Lasker <rashidlasker@gmail.com>
2020-04-27 21:30:26 -0400

    add index view and names

--- viz/src/App.js
+++ viz/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import jsonData from './data/data'
 import {Line} from 'react-chartjs-2';
 
-const STATES = ['s', 'r', 'i', 'a', 'e', 'h', 'f', 'q', 'p'];
+const STATE_MAPPING = {
+  's': 'Susceptible', 
+  'e': 'Exposed', 
+  'i': 'Infected', 
+  'p': 'Preventative Isolation',
+  'a': 'Asymptomatic Isolation', 
+  'q': 'Quarantined', 
+  'h': 'Requires Hospitalization', 
+  'r': 'Recovered', 
+  'f': 'Fatality' 
+}
+
+const STATES = Object.keys(STATE_MAPPING);
 
 const generateColor = function() {
   var r = Math.floor(Math.random() * 255);
@@ -14,31 +25,21 @@ const generateColor = function() {
 };
 
 function App() {
-
-  var labels = [...Array(366).keys()];
+  var labels = [...Array(Object.keys(jsonData).length).keys()];
 
   var datasets = [];
   STATES.forEach(state => {
     var stateData = [];
     for (const i in jsonData) {
-      // stateData.push({"x": parseInt(i), "y": jsonData[i][state + '.num']})
       stateData.push(jsonData[i][state + '.num'])
     }
-    const color = generateColor()
+    const color = generateColor();
     datasets.push({
-      label: state,
+      label: STATE_MAPPING[state],
       fill: false,
-      lineTension: 0.1,
       backgroundColor: color,
       borderColor: color,
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
       pointBorderColor: color,
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
       pointHoverBackgroundColor: color,
       pointHoverBorderColor: 'rgba(220,220,220,1)',
       pointHoverBorderWidth: 2,
@@ -47,15 +48,44 @@ function App() {
       data: stateData
     })
   });
+  
+  const options = {
+    responsive: true,
+    title: {
+      display: true,
+      text: 'COVID19 Chart'
+    },
+    tooltips: {
+      mode: 'index',
+      intersect: false,
+    },
+    scales: {
+      x: {
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Day'
+        }
+      },
+      y: {
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Number of People'
+        }
+      }
+    }
+  }
+
   const data = {
-    labels: labels,
-    datasets: datasets,
+    labels,
+    datasets,
   };
   console.log(data)
   return (
     <div className="App">
       <header className="App-header">
-        <Line data={data} />
+        <Line data={data} options={options}/>
       </header>
     </div>
   );
