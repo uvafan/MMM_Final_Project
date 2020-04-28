81261e3f2c0148ff0784ef8a42b6b38dcfe872ed
Rashid Lasker <rashidlasker@gmail.com>
2020-04-28 01:36:40 -0400

    add tooltip placeholders

--- viz/src/InteractiveSimulator.js
+++ viz/src/InteractiveSimulator.js
@@ -3,6 +3,8 @@ import { API_URL } from './constants';
 import LineChart from './LineChart';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Tooltip from '@material-ui/core/Tooltip';
 import Slider from '@material-ui/core/Slider';
 
 const containerStyles = makeStyles({
@@ -20,6 +22,8 @@ const sliderStyles = makeStyles({
 });
 
 const InteractiveSimulator = () => {
+  // TODO: Add Tooltip text
+  // TODO: Change defaults, step, ranges
   const [jsonData, setJsonData] = useState({});
   const [isolationMultiplier, setIsolationMultiplier] = React.useState(1);
   const [contactTracingMultiplier, setContactTracingMultiplier] = React.useState(1);
@@ -65,14 +69,24 @@ const InteractiveSimulator = () => {
   const sliderProps = {
     className: sliderClasses.root,
     valueLabelDisplay: "auto",
- }
+  }
+
+  const SliderTitle = ({label, tooltipText}) => (
+    <Tooltip title={tooltipText} placement="top" arrow>
+      <Typography variant="h6">
+        {label}
+      </Typography>
+    </Tooltip>
+  );
   
   return (
     <>
+      <Typography variant="h2">
+        Interactive Simulator
+      </Typography>
+      <Box my={4}>
       <div className={containerClasses.root}>
-        <Typography>
-          Isolation Multiplier
-        </Typography>
+        <SliderTitle label="Isolation Multiplier" tooltipText="Add" />
         <Slider
           {...sliderProps}
           marks
@@ -82,33 +96,7 @@ const InteractiveSimulator = () => {
           max={10}
           onChangeCommitted={(event, newValue) => {setIsolationMultiplier(newValue);}}
         />
-        <Typography>
-          Contact Tracing Multiplier
-        </Typography>
-        <Slider
-          {...sliderProps}
-          marks
-          defaultValue={5}
-          step={1}
-          min={1}
-          max={10}
-          onChangeCommitted={(event, newValue) => {setContactTracingMultiplier(newValue);}}
-        />
-        <Typography>
-          Physical Distancing Multiplier
-        </Typography>
-        <Slider
-          {...sliderProps}
-          marks
-          defaultValue={5}
-          step={1}
-          min={1}
-          max={10}
-          onChangeCommitted={(event, newValue) => {setPhysicalDistancingMultiplier(newValue);}}
-        />
-        <Typography>
-          Hospital Capacity
-        </Typography>
+        <SliderTitle label="Hospital Capacity" tooltipText="Add" />
         <Slider
           {...sliderProps}
           marks
@@ -118,11 +106,7 @@ const InteractiveSimulator = () => {
           max={10}
           onChangeCommitted={(event, newValue) => {setHospitalCapacity(newValue);}}
         />
-      </div>
-      <div className={containerClasses.root}>
-        <Typography>
-          Lockdown Days
-        </Typography>
+        <SliderTitle label="Lockdown Days" tooltipText="Add" />
         <Slider
           {...sliderProps}
           marks
@@ -132,9 +116,7 @@ const InteractiveSimulator = () => {
           max={10}
           onChangeCommitted={(event, newValue) => {setLockdownDays(newValue);}}
         />
-        <Typography>
-          Lockdown Start
-        </Typography>
+        <SliderTitle label="Lockdown Start" tooltipText="Add" />
         <Slider
           {...sliderProps}
           marks
@@ -144,9 +126,30 @@ const InteractiveSimulator = () => {
           max={10}
           onChangeCommitted={(event, newValue) => {setLockdownStart(newValue);}}
         />
-        <Typography>
-          Mask Multiplier
-        </Typography>
+      </div>
+      <div className={containerClasses.root}>
+        
+        <SliderTitle label="Contact Tracing Multiplier" tooltipText="Add" />
+        <Slider
+          {...sliderProps}
+          marks
+          defaultValue={5}
+          step={1}
+          min={1}
+          max={10}
+          onChangeCommitted={(event, newValue) => {setContactTracingMultiplier(newValue);}}
+        />
+        <SliderTitle label="Physical Distancing Multiplier" tooltipText="Add" />
+        <Slider
+          {...sliderProps}
+          marks
+          defaultValue={5}
+          step={1}
+          min={1}
+          max={10}
+          onChangeCommitted={(event, newValue) => {setPhysicalDistancingMultiplier(newValue);}}
+        />
+        <SliderTitle label="Mask Multiplier" tooltipText="Add" />
         <Slider
           {...sliderProps}
           marks
@@ -157,7 +160,7 @@ const InteractiveSimulator = () => {
           onChangeCommitted={(event, newValue) => {setMaskMultiplier(newValue);}}
         />
       </div>
-      
+      </Box>
       <LineChart jsonData={jsonData}/>
     </>
   );
