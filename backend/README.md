cc99c8b20c39f57fed68f3ee493470d3726c4ef8
Rashid Lasker <rashidlasker@gmail.com>
2020-04-28 15:02:25 -0400

    maybe this will work

--- backend/README.md
+++ backend/README.md
@@ -8,7 +8,7 @@ gpg --keyserver keyserver.ubuntu.com --recv-key E298A3A825C0D65DFD57CBB651716619
 gpg -a --export E084DAB9 | sudo apt-key add -
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install r-base r-base-dev nginx
+sudo apt-get install r-base r-base-dev
 sudo Rscript install.R 
 sudo apt install python3-pip gunicorn3
 pip3 install -r requirements.txt
--- backend/install.R
+++ backend/install.R
@@ -2,6 +2,24 @@
 for (pkg in c("tidyverse", "magrittr", "lubridate", "knitr", 
               "gt", "devtools", "DiagrammeR", "EpiModel", 
               "parallel", "foreach", "tictoc", "patchwork", "future")) 
-  if (!requireNamespace(pkg)) install.packages(pkg)
+  if (!requireNamespace(pkg)) install.packages(pkg, dependencies=TRUE)
 
 if (!requireNamespace("gt")) install_github("gt")
+
+## ----setup, include=FALSE, eval=TRUE---------------------------------------------------
+knitr::opts_chunk$set(echo = FALSE, cache=TRUE,
+                      tidy.opts=list(width.cutoff=60),
+                      tidy=TRUE)
+
+suppressMessages(library(tidyverse, quietly = T))
+suppressMessages(library(patchwork))
+suppressMessages(library(magrittr))
+suppressMessages(library(lubridate))
+suppressMessages(library(knitr))
+suppressMessages(library(gt))
+suppressMessages(library(tictoc))
+suppressMessages(library(EpiModel))
+suppressMessages(library(DiagrammeR))
+suppressMessages(library(devtools))
+suppressMessages(library(parallel))
+suppressMessages(library(foreach))
